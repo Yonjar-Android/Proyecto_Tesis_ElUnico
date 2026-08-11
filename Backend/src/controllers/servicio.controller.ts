28f7c97e724f0e4acad16c31c0a1ccf0afd914fd
juan <juan132y@gmail.com>
2026-08-11 13:32:13 -0600

    feat: Se implementó el backend de servicios y la parte de la api que conectará luego con las interfaces

---
+++ Backend/src/controllers/servicio.controller.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from "express";
+import {
+    buscarServicios,
+    crearServicio,
+    actualizarServicio
+} from "../services/servicio.service.js"
+
+export const buscarServicio = async(req: Request, res: Response) => {
+    try {
+    
+            const search = req.query.search?.toString() ?? "";
+            const page = Number(req.query.page) || 1;
+            const perPage = Number(req.query.perPage) || 10;
+    
+            const resultado = await buscarServicios(
+                search,
+                page,
+                perPage
+            );
+    
+            res.json(resultado);
+    
+        } catch (error) {
+    
+            res.status(500).json({
+                mensaje: "Error al buscar productos."
+            });
+    
+        }
+}
+
+export const postServicio = async (req: Request, res: Response) => {
+    try{
+
+        const {
+            Nombre_servicio,
+            Descripcion,
+            Precio
+        } = req.body;
+
+        await crearServicio(
+            Nombre_servicio,
+            Descripcion,
+            Precio
+        );
+
+        res.status(201).json({
+            mensaje: "Servicio creado correctamente."
+        });
+
+    } catch (error: any) {
+
+        res.status(400).json({
+            mensaje: error.message
+        });
+
+    }
+}
+
+export const putServicio= async (req: Request, res: Response) => {
+    try {
+
+        const { id } = req.params;
+
+        const {
+            Nombre_servicio,
+            Descripcion,
+            Precio,
+        } = req.body;
+
+        const result = await actualizarServicio(
+            Number(id),
+            Nombre_servicio,
+            Descripcion,
+            Number(Precio),
+        );
+
+        res.status(200).json({
+            mensaje: "Servicio actualizado correctamente.",
+            result
+        });
+
+    } catch (error: any) {
+
+        res.status(400).json({
+            mensaje: error.message
+        });
+
+    }
+};
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import { conectarDB } from "./config/database.js";
 import marcaRoutes from "./routes/marca.routes.js";
 import categoriaRoutes from "./routes/categoria.routes.js";
@@ -12,9 +12,10 @@ import authRoutes from "./routes/auth.routes.js";
 import compraRoutes from "./routes/compra.routes.js";
 import reporteRoutes from "./routes/reporte.routes.js";
 import cajaRoutes from "./routes/caja.routes.js";
+import servicioRoutes from "./routes/service.routes.js"
 import cors from "cors";
 
-dotenv.config();
+//dotenv.config();
 
 const app = express();
 
@@ -34,7 +35,8 @@ app.use("/api/productos", productoRoutes);
 app.use("/api/ventas", ventaRoutes);
 app.use("/api/compras", compraRoutes);
 app.use("/api/caja", cajaRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/servicios", servicioRoutes);
+app.use("/api/auth", authRoutes)
 
 app.use("/api/reportes", reporteRoutes)
 
